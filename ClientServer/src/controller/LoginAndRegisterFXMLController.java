c905f32af2b2f88774b32123a52fa385b3d2f262
Wesam Elmeniawy <wesam.elmeniawy@gmail.com>
2020-12-05 01:24:10 +0200

    Client Side design

---
+++ ClientServer/src/controller/LoginAndRegisterFXMLController.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+
+import helper.ButtonBack;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class LoginAndRegisterFXMLController {
+    
+     /**
+     * backToMainPage.
+     * when called scene will be change to main page.
+     * @param event 
+     */
+    public void backToMainPage(ActionEvent event){
+        
+        System.out.println("backToMainPage: called");
+        
+        ButtonBack btnback = new ButtonBack("/view/sample.fxml");
+        btnback.handleButtonBack(event);
+         
+    }  
+}
